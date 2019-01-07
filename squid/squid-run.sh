eaaaa6be203111dd367ec5e1be1304fe297fc53f
Philipp Defner <philipp.defner@justwatch.com>
2019-01-07 17:54:05 +0100

    Fix path

--- squid/squid-run.sh
+++ squid/squid-run.sh
@@ -4,7 +4,7 @@
 echo "Starting Squid"
 
 # Prevent stale PID (https://github.com/phusion/baseimage-docker/issues/227)
-rm -rf /var/run/squid3.pid
+rm -rf /var/run/squid.pid
 
 #svwaitup 10 /service/openvpn || exit 1
 exec squid -sN
